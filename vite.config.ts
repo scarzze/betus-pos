2c63ba99dbbc3bb3c8890dcf0e9c9960e0582694
DanHacks <hydancheru@gmail.com>
2026-02-20 00:44:45 +0300

    removed supabase

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
