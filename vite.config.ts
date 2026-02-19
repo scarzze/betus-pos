4ced5f02300a7c514992e7db1de69f7d587b12b8
Hydan Koech <hydancheru@gmail.com>
2026-02-19 23:55:25 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    host: true,
+    port: 5173,
   },
 })
