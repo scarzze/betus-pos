63d6dac8119388506fce9e8094501a845ba2ce28
Hydan Koech <hydancheru@gmail.com>
2026-02-20 05:40:17 +0300

    Update resizable.tsx

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,35 +1,17 @@
 // src/components/ui/resizable.tsx
 import React from "react";
-import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
-import { GripVertical } from "lucide-react";
-import { cn } from "@/lib/utils";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
-interface ResizableHandleProps extends React.ComponentProps<typeof PanelResizeHandle> {
-  withHandle?: boolean;
-  className?: string;
+interface ResizablePanelGroupProps {
+  direction?: "horizontal" | "vertical";
+  children: React.ReactNode;
 }
 
-export const ResizablePanelGroup: React.FC<React.ComponentProps<typeof PanelGroup>> = ({
-  className,
-  ...props
-}) => (
-  <PanelGroup className={cn("flex h-full w-full data-[panel-group-direction=vertical]:flex-col", className)} {...props} />
+export const ResizablePanelGroup: React.FC<ResizablePanelGroupProps> = ({ direction = "horizontal", children }) => (
+  <PanelGroup direction={direction}>{children}</PanelGroup>
 );
 
-export const ResizablePanel = Panel;
+export const ResizablePanel: React.FC<{ children: React.ReactNode }> = ({ children }) => <Panel>{children}</Panel>;
 
-export const ResizableHandle: React.FC<ResizableHandleProps> = ({ withHandle, className, ...props }) => (
-  <PanelResizeHandle
-    className={cn(
-      "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 [&[data-panel-group-direction=vertical]>div]:rotate-90",
-      className
-    )}
-    {...props}
-  >
-    {withHandle && (
-      <div className="z-10 flex h-4 w-3 items-center justify-center rounded-sm border bg-border">
-        <GripVertical className="h-2.5 w-2.5" />
-      </div>
-    )}
-  </PanelResizeHandle>
-);
+export const ResizableHandle: React.FC<{ withHandle?: boolean }> = ({ withHandle = true }) =>
+  withHandle ? <PanelResizeHandle className="bg-border w-1 cursor-col-resize" /> : null;
