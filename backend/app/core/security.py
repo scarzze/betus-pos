c1312c8c52a5dea7a6d68b64cb468c22f6efc64e
Hydan Koech <hydancheru@gmail.com>
2026-02-20 03:43:16 +0300

    Update security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,20 +1,22 @@
+from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
-from passlib.context import CryptContext
 from app.core.config import settings
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password: str):
+
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
 
-def verify_password(password, hashed):
-    return pwd_context.verify(password, hashed)
 
-def create_access_token(data: dict):
+def verify_password(plain_password, hashed_password) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict, expires_minutes: int = None):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(
-        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-    )
+    expire = datetime.utcnow() + timedelta(minutes=expires_minutes or settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+    return encoded_jwt
