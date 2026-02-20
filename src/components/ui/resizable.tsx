fad67738eb9827d61bb8281d3d71a61cd177e1f1
Hydan Koech <hydancheru@gmail.com>
2026-02-20 05:29:47 +0300

    Update resizable.tsx

--- src/components/ui/resizable.tsx
+++ src/components/ui/resizable.tsx
@@ -1,27 +1,24 @@
+// src/components/ui/resizable.tsx
+import React from "react";
+import { Panel, PanelGroup, PanelResizeHandle } from "react-resizable-panels";
 import { GripVertical } from "lucide-react";
-import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 import { cn } from "@/lib/utils";
 
-export const ResizablePanelGroup = ({
+interface ResizableHandleProps extends React.ComponentProps<typeof PanelResizeHandle> {
+  withHandle?: boolean;
+  className?: string;
+}
+
+export const ResizablePanelGroup: React.FC<React.ComponentProps<typeof PanelGroup>> = ({
   className,
   ...props
-}: React.ComponentProps<typeof PanelGroup>) => (
-  <PanelGroup
-    className={cn(
-      "flex h-full w-full data-[panel-group-direction=vertical]:flex-col",
-      className
-    )}
-    {...props}
-  />
+}) => (
+  <PanelGroup className={cn("flex h-full w-full data-[panel-group-direction=vertical]:flex-col", className)} {...props} />
 );
 
 export const ResizablePanel = Panel;
 
-export const ResizableHandle = ({
-  withHandle,
-  className,
-  ...props
-}: React.ComponentProps<typeof PanelResizeHandle> & { withHandle?: boolean }) => (
+export const ResizableHandle: React.FC<ResizableHandleProps> = ({ withHandle, className, ...props }) => (
   <PanelResizeHandle
     className={cn(
       "relative flex w-px items-center justify-center bg-border after:absolute after:inset-y-0 after:left-1/2 after:w-1 after:-translate-x-1/2 data-[panel-group-direction=vertical]:h-px data-[panel-group-direction=vertical]:w-full data-[panel-group-direction=vertical]:after:left-0 data-[panel-group-direction=vertical]:after:h-1 data-[panel-group-direction=vertical]:after:w-full data-[panel-group-direction=vertical]:after:-translate-y-1/2 data-[panel-group-direction=vertical]:after:translate-x-0 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-1 [&[data-panel-group-direction=vertical]>div]:rotate-90",
