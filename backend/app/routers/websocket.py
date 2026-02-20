c20440e8fd8367e166b9b0fe410fcfcf66ae2617
Hydan Koech <hydancheru@gmail.com>
2026-02-20 03:18:19 +0300

    Create websocket.py

---
+++ backend/app/routers/websocket.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from app.websocket.manager import manager
+
+router = APIRouter()
+
+@router.websocket("/ws/{org_id}")
+async def websocket_endpoint(websocket: WebSocket, org_id: str):
+    await manager.connect(org_id, websocket)
+
+    try:
+        while True:
+            # keep connection alive
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        manager.disconnect(org_id, websocket)
