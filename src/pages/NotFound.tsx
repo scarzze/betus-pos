afd2d2bbeaeaecaa1a1286fc68db2c81f6d5691c
Hydan Koech <hydancheru@gmail.com>
2026-02-20 03:59:14 +0300

    Update NotFound.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,5 +1,6 @@
 import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { formatCurrency, formatDate } from '@/lib/utils';
 
 const NotFound = () => {
   const location = useLocation();
